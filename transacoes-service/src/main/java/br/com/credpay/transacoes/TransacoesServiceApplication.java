e4cab9b78f06a993326acbbf27e88dc0e343bfa8
Joaomagh <joaomagdev@gmail.com>
2026-07-11 22:02:58 -0300

    chore: scaffold transaction service

---
+++ transacoes-service/src/main/java/br/com/credpay/transacoes/TransacoesServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.credpay.transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransacoesServiceApplication.class, args);
+    }
+}
